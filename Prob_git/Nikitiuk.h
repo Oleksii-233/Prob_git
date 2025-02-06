218901e9736391aff8be60c2db201af74498fe8a
Uliana <ulfweey@gmail.com>
2025-02-06 13:58:15 +0200

    Update Nikitiuk.h

--- Prob_git/Nikitiuk.h
+++ Prob_git/Nikitiuk.h
@@ -6,11 +6,36 @@
 #include "Dar'ev.h"
 using namespace std;
 
-void dozap(fstream file, PeopleInformation){
-cout<<"Äîçàïèñ â ôàéë ";
-  File.open("PeopleInformation.dat", ios::app, ios::binary);
-  file<<"dozapus ";
-  file.close;
+void addtofile(){
+	PeopleInformation inf;
+	ifstream f1("PeopleInformation.dat", ios::binary);
+	ofstream f2("PeopleInformation1.dat", ios::binary);
+	while (f1.read((char*)&inf, sizeof inf))
+		f2.write((char*)&inf, sizeof inf);
+	int num;
+	cout << "К-ть, яку хочете дописати: "; cin >> num; cin.get();
+	cout << "Введите дані:\n";
+	for (i = 0; i <= m - 1; i++)
+	{
+		for (int i = 0; i < NumberPeople; i++) {
+		cout << "Введіть номер людини: "; cin >> inf.Number;
+		cout << "\nВведіть ім'я людини: "; cin.get(inf.Name, 50);
+		cout << "\nВведіть прізвище людини: "; cin.get(inf.Surname, 50);
+		cout << "\nВведіть стать людини: "; cin.get(inf.Sex, 50);
+		cout << "\nВведіть зріст людини: "; cin >> inf.Number;
+		cout << "\nВведіть вагу людини: "; cin >> inf.Number;
+		cout << "\nВведіть номер одягу людини: "; cin >> inf.Number;
+		cout << "\nВведіть номер взуття людини: "; cin >> inf.Number;
+	f2.write((char*)&inf, sizeof inf);
+	}
+	}
+	f1.close();
+	f2.close();
+
+	remove("PeopleInformation.dat");
+	rename("PeopleInformation1.dat", "PeopleInformation.dat");
+
+
 }
 void poshuk(char Surname, int Height, int ClothesNumber, int ShoesNumber, int Weight){
   for(
