a5aa2ed808c158cb92737760d391f25cf20cd5ed
Uliana <ulfweey@gmail.com>
2025-02-06 12:46:13 +0200

    Update Yatsyuk.h

--- Prob_git/Yatsyuk.h
+++ Prob_git/Yatsyuk.h
@@ -1,1 +1,19 @@
 #pragma once
+#indef Yatsiyk
+
+
+void readfromfile(ifstream& file, PeopleInformation inf){
+  file.open("z1.dat");
+  file1.clear();
+	file1.seekg(0);
+  cout <<setw(3)<<"№"<< setw(10)<<<<setw(10)<<"Ім'я"<<setw(8)<<"Стать"<<setw(5)<<"Ріст"<<setw(5)<<"Вага"<<setw(7)<<"№ одежі"<<setw(8)<<"№ взуття"<< endl;	
+		int i=0;
+  while (file.read((char*)&inf, sizeof inf))
+    {
+			  cout <<setw(3)<<i<< setw(10)<<<<setw(10)<<"Ім'я"<<setw(8)<<"Стать"<<setw(5)<<"Ріст"<<setw(5)<<"Вага"<<setw(7)<<"№ одежі"<<setw(8)<<"№ взуття"<< endl;	
+		i++;
+    }
+		file1.close();
+		file1.clear();
+
+}
