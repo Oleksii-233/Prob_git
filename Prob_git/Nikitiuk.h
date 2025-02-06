85e7cf5b90cff7233c48adf7c62c541bfb6ec417
zwinskx <sasha342008@icloud.com>
2025-02-06 12:58:56 +0200

    Update Nikitiuk.h

--- Prob_git/Nikitiuk.h
+++ Prob_git/Nikitiuk.h
@@ -1,4 +1,10 @@
 #pragma once
+#ifndef Nikitiuk
+#define Nikitiuk
+#include <iosrteam>
+#include <fstream>
+#include "Dar'ev.h"
+using namespace std;
 void dozap(fstream file, PeopleInformation){
 cout<<"Дозапис в файл ";
   
