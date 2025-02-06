6b38852d646053d1f75cf8cf479a0a05a932ac07
zwinskx <sasha342008@icloud.com>
2025-02-06 13:14:26 +0200

    Update Nikitiuk.h

--- Prob_git/Nikitiuk.h
+++ Prob_git/Nikitiuk.h
@@ -5,7 +5,11 @@
 #include <fstream>
 #include "Dar'ev.h"
 using namespace std;
+
 void dozap(fstream file, PeopleInformation){
 cout<<"Дозапис в файл ";
   File.open("PeopleInformation.dat", ios::app, ios::binary);
+  file<<"dozapus ";
+  file.close;
 }
+void 
