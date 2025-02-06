ef6498cd15d3c547a41912ae6e74681b54140308
Uliana <ulfweey@gmail.com>
2025-02-06 13:05:57 +0200

    Update Yatsyuk.h

--- Prob_git/Yatsyuk.h
+++ Prob_git/Yatsyuk.h
@@ -22,3 +22,22 @@ File.open("PeopleInformation.dat", ios::in, ios::binary);
 		file.clear();
 
 }
+
+void sortbysize(ifstream& file){
+PeopleInformation inf;
+File.open("PeopleInformation.dat", ios::in, ios::binary);
+  file.clear();
+	file.seekg(0);
+	if(inf.ClothesNumber>46 && inf.ShoesNumber<41){
+	cout<<"Прізвища людей, в яких ном. одягу більше 46 та розмір взуття менше 41:\n";	
+  while (file.read((char*)&inf, sizeof inf))
+    {
+	cout <<setw(3)<<inf.Number<< setw(10)<<inf.Surname<<setw(10)<<inf.Name<<setw(8)<<inf.Sex<<setw(5)<<inf.Height<<setw(5)<<inf.Weight<<setw(7)<<inf.ClothesNumber<<setw(8)<<inf.ShoesNumber<< endl;	
+		
+    }
+	}
+		file.close();
+		file.clear();
+
+}
+}
