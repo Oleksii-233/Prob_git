09a81dc9130e00c531a84f9303e2c72e4d6db094
Oleksii-233 <bashuk0325oleksij@gmail.com>
2025-02-06 13:57:25 +0200

    Синхронізація № 2

--- Prob_git/Yatsyuk.h
+++ Prob_git/Yatsyuk.h
@@ -16,10 +16,10 @@ file.open("PeopleInformation.dat", ios::in, ios::binary);
   cout <<setw(3)<<"№"<< setw(10)<<setw(10)<<"Ім'я"<<setw(8)<<"Стать"<<setw(5)<<"Ріст"<<setw(5)<<"Вага"<<setw(7)<<"№ одежі"<<setw(8)<<"№ взуття"<< endl;	
 	
   while (file.read((char*)&inf, sizeof inf))
-    {
+  {
 	cout <<setw(3)<<inf.Number<< setw(10)<<inf.Surname<<setw(10)<<inf.Name<<setw(8)<<inf.Sex<<setw(5)<<inf.Height<<setw(5)<<inf.Weight<<setw(7)<<inf.ClothesNumber<<setw(8)<<inf.ShoesNumber<< endl;	
 		
-    }
+  }
 		file.close();
 		file.clear();
 
