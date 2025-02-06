a473f18149be2a4cc085a85dc390374e6e5dcce6
Uliana <ulfweey@gmail.com>
2025-02-06 12:49:07 +0200

    Update Yatsyuk.h

--- Prob_git/Yatsyuk.h
+++ Prob_git/Yatsyuk.h
@@ -7,11 +7,11 @@ void readfromfile(ifstream& file, PeopleInformation inf){
   file1.clear();
 	file1.seekg(0);
   cout <<setw(3)<<"№"<< setw(10)<<<<setw(10)<<"Ім'я"<<setw(8)<<"Стать"<<setw(5)<<"Ріст"<<setw(5)<<"Вага"<<setw(7)<<"№ одежі"<<setw(8)<<"№ взуття"<< endl;	
-		int i=0;
+	
   while (file.read((char*)&inf, sizeof inf))
     {
-			  cout <<setw(3)<<i<< setw(10)<<<<setw(10)<<"Ім'я"<<setw(8)<<"Стать"<<setw(5)<<"Ріст"<<setw(5)<<"Вага"<<setw(7)<<"№ одежі"<<setw(8)<<"№ взуття"<< endl;	
-		i++;
+	cout <<setw(3)<<inf.Number<< setw(10)<<inf.Surname<<setw(10)<<inf.Name<<setw(8)<<inf.Sex<<setw(5)<<inf.Height<<setw(5)<<inf.Weight<<setw(7)<<inf.ClothesNumber<<setw(8)<<inf.ShoesNumber<< endl;	
+		
     }
 		file1.close();
 		file1.clear();
