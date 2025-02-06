7ac8faed8c3f524ef5e4d2b8b1a6984b2d595203
Oleksii-233 <bashuk0325oleksij@gmail.com>
2025-02-06 11:27:21 +0200

    Зведено до пустого

--- Prob_git/Dar`ev.h
+++ Prob_git/Dar`ev.h
@@ -1,14 +1,1 @@
 #pragma once
-#ifndef Dariev
-#define Dariev
-#include <iostream>
-using namespace std;
-
-void Oleksiy();
-
-void Oleksiy() {
-	for(int i=0 ; i<999999;i++)
-	cout << "Chort" << endl;
-}
-
-#endif
--- Prob_git/Yatsyuk.h
+++ Prob_git/Yatsyuk.h
@@ -1,15 +1,1 @@
 #pragma once
-#ifndef Yatsiyk
-#define Yatsiyk
-#include <iostream>
-using namespace std;
-
-void a();
-
-void a() {
-	for(int i=0 ; i<10;i++)
-	cout << "Хочу літо" << endl;
-}
-
-#endif
-
