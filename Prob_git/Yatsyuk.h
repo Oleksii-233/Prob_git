fcffd0b6ba79786ff7f4a33c90c0288676c5e23d
Oleksii-233 <bashuk0325oleksij@gmail.com>
2025-02-08 16:26:32 +0200

    Продовження  йде редагування

--- Prob_git/Yatsyuk.h
+++ Prob_git/Yatsyuk.h
@@ -9,34 +9,45 @@
 #include "Dar`ev.h"
 
 void readfromfile(fstream& file){
- PeopleInformation inf;
-file.open("PeopleInformation.dat", ios::in, ios::binary);
-  file.clear();
-	file.seekg(0);
-  cout <<setw(3)<<"№"<< setw(10)<<setw(10)<<"Ім'я"<<setw(8)<<"Стать"<<setw(5)<<"Ріст"<<setw(5)<<"Вага"<<setw(7)<<"№ одежі"<<setw(8)<<"№ взуття"<< endl;	
-	
-  while (file.read((char*)&inf, sizeof(inf)))
-  {
-	cout <<setw(3)<<inf.Number<< setw(10)<<inf.Surname<<setw(10)<<inf.Name<<setw(8)<<inf.Sex<<setw(5)<<inf.Height<<setw(5)<<inf.Weight<<setw(7)<<inf.ClothesNumber<<setw(8)<<inf.ShoesNumber<< endl;	
-		
-  }
-file.close();
-file.clear();
+PeopleInformation inf;
 
+file.open("PeopleInformation.dat", ios::in, ios::binary);
+
+if (!file.is_open()) {
+	cout << "Не вдалось відкрити файл." << endl;
+	exit(1);
+}
+
+  Shapka();	
+  while (file.read((char*)&inf, sizeof(inf)))
+	  ShowPeople(inf);
+		
+file.close();
 }
 
 void sortbysize(fstream& file){
 PeopleInformation inf;
+int k = 0;
 file.open("PeopleInformation.dat", ios::in, ios::binary);
-  file.clear();
-	file.seekg(0);	
-	cout<<"Прізвища людей, в яких ном. одягу більше 46 та розмір взуття менше 41:\n";	
+
+if (!file.is_open()) {
+	cout << "Не вдалось відкрити файл." << endl;
+	exit(1);
+}
 	
+cout<<"Прізвища людей, в яких ном. одягу більше 46 та розмір взуття менше 41:\n";		
   while (file.read((char*)&inf, sizeof(inf)))
-  { if(inf.ClothesNumber>46 && inf.ShoesNumber<41){
-	    cout<<inf.Number<<"\t"<<inf.Surname<<endl;}
+  { 
+	  if(inf.ClothesNumber>46 && inf.ShoesNumber<41){
+		  cout << inf.Number << "\t" << inf.Surname << endl;
+	  k++;
+	  }
   }
-		file.close();
+
+  if (k == 0) 
+	  cout << "Відсутні..." << endl;
+  
+   file.close();
 
 }
 
